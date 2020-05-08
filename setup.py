03119e86e224d085273247434ffc53782528486b
Amged Rustom <amgadhs@codehill.com>
2020-05-08 23:06:29 +0400

    Add Python 3.7 and 3.8 to list of tested versions

--- setup.py
+++ setup.py
@@ -29,6 +29,8 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: Site Management",
