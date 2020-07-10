cbb66e171e3a179cf000bf77e8d8546be089bc46
Amged Rustom <amgadhs@codehill.com>
2020-07-10 14:33:27 +0400

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='webchk',
-    version='1.0.2',
+    version='1.0.3',
     packages=['webchk'],
     test_suite='test',
     url='https://codehill.com/projects/webchk',
